736f85f6b9f57644dd4928559e1047aac361831c
Uriel Padiglione <uriel.padiglione@gmail.com>
2023-10-20 00:55:20 +0200

    Removed left walker; added walkable map

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rltk::{Rltk, GameState, RGB, VirtualKeyCode};
+use rltk::{Rltk, GameState, RGB, VirtualKeyCode, Tile};
 use specs::prelude::*;
 use std::cmp::{max, min};
 use specs_derive::Component;
@@ -7,8 +7,8 @@ use specs_derive::Component;
 struct State{ecs: World}
 impl State {
     fn run_systems(&mut self){
-        let mut lw = LeftWalker{};
-        lw.run_now(&self.ecs);
+       //let mut lw = LeftWalker{};
+       //lw.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
@@ -34,7 +34,7 @@ struct Renderable {
 #[derive(Component, Debug)]
 struct Player {}
 
-#[derive(Component)]
+/*#[derive(Component)]
 struct LeftMover {}
 
 struct LeftWalker {}
@@ -48,7 +48,7 @@ impl<'a> System<'a> for LeftWalker{
             if pos.x < 0 { pos.x = 79}
         }
     }
-}
+}*/
 
 impl GameState for State{
     fn tick(&mut self, ctx : &mut Rltk){
@@ -56,6 +56,9 @@ impl GameState for State{
         
         player_input(self, ctx);
         self.run_systems();
+        
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
     
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -79,8 +82,8 @@ fn main() -> rltk::BError{
     let mut gs = State { ecs: World::new() };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
-    gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
+    gs.ecs.insert(new_map());
 
     // BUILDER PATTERN! very common in rust.
     gs.ecs
@@ -94,7 +97,7 @@ fn main() -> rltk::BError{
         .with(Player{})
         .build();
 
-    for i in 0..10 {
+/*     for i in 0..10 {
         gs.ecs
             .create_entity()
             .with(Position {x:i * 7, y: 20})
@@ -103,9 +106,8 @@ fn main() -> rltk::BError{
                 fg: RGB::named(rltk::RED),
                 bg: RGB::named(rltk::BLACK),
             })
-            .with(LeftMover{})
             .build();
-    }
+    } */
 
     rltk::main_loop(context, gs)
 }
@@ -114,10 +116,14 @@ fn main() -> rltk::BError{
 fn try_move_player(delta_x:i32, delta_y:i32,ecs: &mut World){
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
 
     for(_player, pos) in (&mut players, &mut positions).join(){
-        pos.x = min(79, max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
+        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map[destination_idx] != TileType::Wall {
+            pos.x = min(79, max(0, pos.x + delta_x));
+            pos.y = min(49, max(0, pos.y + delta_y));
+        }
     }
 }
 
@@ -136,4 +142,57 @@ fn player_input(gs: &mut State, ctx: &mut Rltk){
 
 pub fn xy_idx(x: i32, y: i32) -> usize {
     (y as usize * 80) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor;80*50];
+
+    // boundaries walls
+    for x in 0..80{
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, 49)] = TileType::Wall;
+    }
+
+    for y in 0..50 {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
+
+    // randomly splatting some ugly  walls.2
+    // getting the thread-local RNG first...
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    for _i in 0..400 {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let idx = xy_idx(x, y);
+        if idx != xy_idx(40, 25) {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    return map
+}
+
+fn draw_map(map: &[TileType], ctx : &mut Rltk) {
+    let mut y = 0;
+    let mut x = 0;
+    for tile in map.iter() {
+        // Render a tile depending upon the tile type
+        match tile {
+            TileType::Floor => {
+                ctx.set(x, y, RGB::from_f32(0.5, 0.5, 0.5), RGB::from_f32(0., 0., 0.), rltk::to_cp437('.'));
+            }
+            TileType::Wall => {
+                ctx.set(x, y, RGB::from_f32(0.0, 1.0, 0.0), RGB::from_f32(0., 0., 0.), rltk::to_cp437('#'));
+            }
+        }
+
+        // Move the coordinates
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
+    }
 }
